3111e59b2455002e159da8ace603f8520f9d6381
☃ Elliot Shepherd <elliot@ninjablocks.com>
2014-04-22 14:46:21 +1000

    Attribute discovery and reporting, attribute data-types xml

--- tests/list-devices.js
+++ tests/list-devices.js
@@ -25,6 +25,10 @@ glob('{/dev/tty.zigbee,/dev/cu.usbmodem*}', function (err, devices) {
       console.log('CC2530/1 firmware version: %s %s', version.type, versionString);
 
     })
+    /*/ reset our device so we get back to a clean state
+    .then(function() {
+      client.resetDevice(false);
+    })//*/
     .then(client.startCoordinator.bind(client))
     .then(function() {
 
@@ -46,12 +50,66 @@ glob('{/dev/tty.zigbee,/dev/cu.usbmodem*}', function (err, devices) {
 
                 clusters.forEach(function(cluster) {
                   console.log('Found', cluster.toString());
+                  /*cluster.discoverAttributes(0, 100).then(function(attributes) {
+                    console.log('Attributes', cluster.toString(), attributes);
+                  });*/
+
+                  cluster.readAttributes().then(function(values) {
+                    console.log('Attributes', cluster.toString(), values);
+                  }).catch(function(e) {
+                    console.log('Attributes read fail', cluster.toString(), e.stack);
+                  });
+
+                  /*try {
+                    cluster.readReportingConfiguration().then(function(values) {
+                      console.log('Read reporting', cluster.toString(), values);
+                    }).catch(function(e) {
+                      console.log('Read reporting fail', cluster.toString(), e);
+                    });
+                  } catch(e) {
+                    console.error('sss', e.stack);
+                  }*/
+
                 });
 
                 var colorCluster = _.findWhere(clusters, {name: 'Color Control'});
                 var onOffCluster = _.findWhere(clusters, {name: 'On/Off'});
+                var meteringCluster = _.findWhere(clusters, {name: 'Simple Metering'});
 
-                if (colorCluster) {
+                if (meteringCluster) {
+                 /* meteringCluster.discoverAttributes(0, 100).then(function(attributes) {
+                    console.log('Attributes', meteringCluster.toString(), attributes);
+                  });
+
+                  meteringCluster.attributes.InstantaneousDemand.read(function(value) {
+                    console.log('InstantaneousDemand value', meteringCluster.toString(), value);
+                  });
+
+                  meteringCluster.reportAttributes({
+                    id:'InstantaneousDemand',
+                    minimumReportingInterval: 10,
+                    maximumReportingInterval: 20,
+                    reportableChange: 5
+                  }).then(function(response) {
+                    console.log('Report InstantaneousDemand response', meteringCluster.toString(), response);
+                  });*/
+                }
+
+                /*if (onOffCluster) {
+                  onOffCluster.discoverAttributes(0, 100).then(function(attributes) {
+                    console.log('Attributes', onOffCluster.toString(), attributes);
+                  });
+
+                  onOffCluster.reportAttributes({
+                    id:'OnOff',
+                    minimumReportingInterval: 1,
+                    maximumReportingInterval: 120
+                  }).then(function(response) {
+                    console.log('Report OnOff response', onOffCluster.toString(), response);
+                  });
+                }*/
+
+                /*if (colorCluster) {
 
                   console.log('Alternating colours', colorCluster.toString());
                   setInterval(function() {
@@ -67,7 +125,7 @@ glob('{/dev/tty.zigbee,/dev/cu.usbmodem*}', function (err, devices) {
                     onOffCluster.commands.Toggle().done();
                   }, 3000);
 
-                }
+                }*/
 
               });
 
@@ -75,6 +133,8 @@ glob('{/dev/tty.zigbee,/dev/cu.usbmodem*}', function (err, devices) {
             device.findActiveEndpoints();
             device.findEndpoints(0x0104, [0x0500], [0x0500]); // HA IAS Zones.
           });
+        }).catch(function(e) {
+          console.error('Failed getting devices', e.stack);
         });
       }, 5000);
       
